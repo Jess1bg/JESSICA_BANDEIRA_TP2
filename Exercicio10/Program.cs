bf8f5b68ea5141b70c508a6f2f312aac44476730
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-28 03:46:12 -0300

    TP2

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número inteiro: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        for (int i = numero; i >= 0; i--)
+        {
+            Console.Write(i);
+            if (i > 0) Console.Write(", ");
+        }
+    }
+}
