1d06552f807356f1c1650863e03282b55e41aeb4
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-28 02:47:45 -0300

    TP2

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Idade: ");
+        string idade = Console.ReadLine();
+
+        Console.Write("Telefone: ");
+        string telefone = Console.ReadLine();
+
+        Console.Write("E-mail: ");
+        string email = Console.ReadLine();
+
+        Console.WriteLine("\nDados cadastrados:");
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Idade: {idade}");
+        Console.WriteLine($"Telefone: {telefone}");
+        Console.WriteLine($"E-mail: {email}");
+    }
+}
