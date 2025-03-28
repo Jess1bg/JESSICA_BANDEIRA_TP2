3b7fe95389596b785099db1ea572b1cef2ef9bb8
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-28 03:33:30 -0300

    TP2

---
+++ Exercicio08/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite sua nota de 0 a 10: ");
+        double nota = double.Parse(Console.ReadLine());
+
+        string classificacao;
+
+        if (nota < 5)
+            classificacao = "Infelizmente sua nota é insuficiente";
+        else if (nota < 7)
+            classificacao = "Sua nota é regular";
+        else if (nota < 9)
+            classificacao = "Bom";
+        else
+            classificacao = "Parabens sua nota foi excelente";
+
+        Console.WriteLine($"Classificação: {classificacao}");
+    }
+}
