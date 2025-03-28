aa6bca6375adcf6b24264cf8b545d133309cbeac
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-28 02:31:53 -0300

    TP2

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Insira a sua data de nascimento (dd/mm/aaaa): ");
+        DateTime dataNascimento;
+
+        while (!DateTime.TryParse(Console.ReadLine(), out dataNascimento))
+        {
+            Console.Write("Formato incorreto. Tente novamente (dd/mm/aaaa): ");
+        }
+
+        Console.WriteLine("-----------------------------"); 
+
+        DateTime hoje = DateTime.Today;
+        DateTime proximoAniversario = new DateTime(hoje.Year, dataNascimento.Month, dataNascimento.Day);
+
+        if (proximoAniversario < hoje)
+        {
+            proximoAniversario = proximoAniversario.AddYears(1);
+        }
+
+        int diasRestantes = (proximoAniversario - hoje).Days;
+        Console.WriteLine($"Faltam {diasRestantes} dias para o seu próximo aniversário.");
+    }
+}
