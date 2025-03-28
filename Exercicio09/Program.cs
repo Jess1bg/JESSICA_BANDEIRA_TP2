d5260cc1104bade9e58b4b68a61fd0655b3608cc
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-28 03:40:00 -0300

    TP2

---
+++ Exercicio09/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Por favor, digite o valor do seu salário bruto: ");
+        double salarioBruto = double.Parse(Console.ReadLine());
+        double desconto = 0;
+
+        if (salarioBruto <= 2000)
+            desconto = salarioBruto * 0.05;
+        else if (salarioBruto <= 4000)
+            desconto = salarioBruto * 0.12 - 140;
+        else if (salarioBruto <= 6000)
+            desconto = salarioBruto * 0.18 - 380;
+        else
+            desconto = salarioBruto * 0.25 - 650;
+
+        double salarioLiquido = salarioBruto - desconto;
+
+        Console.WriteLine($"\nSalário Bruto: R$ {salarioBruto:F2}");
+        Console.WriteLine($"Desconto de Impostos: R$ {desconto:F2}");
+        Console.WriteLine($"Salário Líquido: R$ {salarioLiquido:F2}");
+    }
+}
