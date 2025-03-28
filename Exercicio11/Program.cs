dc943ec43d94392124d7c4f0deb945d62cbe876c
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-28 03:50:35 -0300

    TP2

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número para ver a tabuada: ");
+        int numero = int.Parse(Console.ReadLine());
+        int i = 1;
+        while (i <= 10)
+        {
+            Console.WriteLine($"{numero} x {i} = {numero * i}");
+            i++;
+        }
+    }
+}
